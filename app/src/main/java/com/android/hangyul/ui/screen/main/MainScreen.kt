7863112d05bcf38008ff85298bea1692a5327477
yeochaeeon <codjs2659@pusan.ac.kr>
2025-06-04 23:40:26 +0900

    style:메인페이지 일부

--- app/src/main/java/com/android/hangyul/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/android/hangyul/ui/screen/main/MainScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.android.hangyul.ui.theme.HangyulTheme
 
 @Composable
 fun MainScreen() {
@@ -12,6 +14,18 @@ fun MainScreen() {
         modifier = Modifier
             .padding(horizontal = 24.dp, vertical = 16.dp)
     ) {
+        EmotionAnalysisCard(
+            "오늘은 조금 지친 마음이 느껴졌어요",
+            "오늘도 잘 버텼어요. 마음이 괜찮아 질거예요.",
+            "5/27(화)",
+            onClick = {})
         FeatureButtonGrid()
+    }
+}
+@Composable
+@Preview
+fun MainScreenPreview(){
+    HangyulTheme {
+        MainScreen()
     }
 }
