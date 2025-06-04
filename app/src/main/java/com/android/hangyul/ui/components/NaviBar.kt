743b1522a833c778cc8f267ecdc5ab35f46a9cea
latteeea <kimtaeran4767@gmail.com>
2025-06-04 20:54:01 +0900

    chore: pretendard font 세팅

--- app/src/main/java/com/android/hangyul/ui/components/NaviBar.kt
+++ app/src/main/java/com/android/hangyul/ui/components/NaviBar.kt
@@ -1,4 +1,39 @@
 package com.android.hangyul.ui.components
 
-class NaviBar {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.android.hangyul.R
+import com.android.hangyul.ui.theme.Purple
+import com.android.hangyul.ui.theme.Blue
+
+@Composable
+fun NaviBar() {
+
+
 }
+
+@Preview
+@Composable
+fun NaviBarPreview() {
+    NaviBar()
+}
+
