31a06f0fc7698fc2e1c9a6ff8e4fce4638e5fb4e
yeochaeeon <codjs2659@pusan.ac.kr>
2025-06-06 10:58:18 +0900

    chore:지난일기 보기 카드 코틀린 생성

--- app/src/main/java/com/android/hangyul/ui/screen/braintraining/ConvertedDiaryCard.kt
+++ app/src/main/java/com/android/hangyul/ui/screen/braintraining/ConvertedDiaryCard.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.android.hangyul.R
-import com.android.hangyul.ui.screen.diary.DiaryPage
 import com.android.hangyul.ui.theme.HangyulTheme
 
 @Composable
---
+++ app/src/main/java/com/android/hangyul/ui/screen/braintraining/DiaryHistoryCard.kt
@@ -0,0 +1,2 @@
+package com.android.hangyul.ui.screen.braintraining
+
