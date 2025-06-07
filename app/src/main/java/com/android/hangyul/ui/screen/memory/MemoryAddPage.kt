e652313f2865d22a76139aa4c31e34243cc7ee61
yeochaeeon <codjs2659@pusan.ac.kr>
2025-06-08 02:37:48 +0900

    chore:텍스트 입력창 디자인 개선

--- app/src/main/java/com/android/hangyul/ui/screen/memory/MemoryAddPage.kt
+++ app/src/main/java/com/android/hangyul/ui/screen/memory/MemoryAddPage.kt
@@ -10,9 +10,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,6 +23,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
@@ -27,6 +32,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.android.hangyul.R
+import com.android.hangyul.ui.components.AddInputField
 import com.android.hangyul.viewmodel.Memory
 import com.android.hangyul.viewmodel.MemoryViewModel
 
@@ -65,25 +71,70 @@ fun MemoryAddPage(viewModel: MemoryViewModel, onSave: ()->Unit) {
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        OutlinedTextField(
+        TextField(
             value = title,
             onValueChange = { title = it },
             label = { Text("제목") },
-            modifier = Modifier.align(Alignment.CenterHorizontally),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .shadow(
+                    elevation = 12.dp,
+                    shape = RoundedCornerShape(12.dp)
+                ),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
+                disabledContainerColor = Color.White,
+                errorContainerColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                errorIndicatorColor = Color.Transparent
+            )
         )
-        Spacer(modifier = Modifier.height(10.dp))
-        OutlinedTextField(
+        Spacer(modifier = Modifier.height(15.dp))
+        TextField(
             value = date,
             onValueChange = { date = it },
             label = { Text("날짜") },
-            modifier = Modifier.align(Alignment.CenterHorizontally),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .shadow(
+                    elevation = 12.dp,
+                    shape = RoundedCornerShape(12.dp)
+                ),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
+                disabledContainerColor = Color.White,
+                errorContainerColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                errorIndicatorColor = Color.Transparent
+            )
         )
-        Spacer(modifier = Modifier.height(10.dp))
-        OutlinedTextField(
+        Spacer(modifier = Modifier.height(15.dp))
+        TextField(
             value = content,
             onValueChange = { content = it },
             label = { Text("내용") },
-            modifier = Modifier.align(Alignment.CenterHorizontally),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .shadow(
+                    elevation = 12.dp,
+                    shape = RoundedCornerShape(12.dp)
+                ),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
+                disabledContainerColor = Color.White,
+                errorContainerColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                errorIndicatorColor = Color.Transparent
+            )
         )
 
 
